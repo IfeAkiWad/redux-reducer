07a661a1144688adb177c96fca3ffe01333509d6
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2021-09-08 10:54:25 -0500

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -5,6 +5,8 @@ export function managePresents(state, action){
     switch (action.type) {
         case "INCREASE":
           return { numberOfPresents: state.numberOfPresents + 1 };
+        default:
+            return state
     }
 
 }
