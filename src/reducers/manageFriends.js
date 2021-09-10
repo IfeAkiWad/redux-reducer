27612787c9a4d5d423741d302b11306da366edde
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2021-09-10 13:26:03 -0500

    commit tree abdf37f57a4f4e09aba3fd17369de8418d09fdd6

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,9 +1,12 @@
 export function manageFriends(state = {friends: []}, action){
     switch (action.type) {
         case "ADD_FRIEND":
-          return { friends: state.friends + 1 };
+          // return { friends: state.friends + 1 };
+          return {...state, friends: [...state.friends, action.friend]}
         case "REMOVE_FRIEND":
-          return { friends: state.friends - 1 };
+          // return { friends: state.friends - 1 };
+          const newFriends = state.friends.filter(friend => friend.id !== action.id)
+          return {...state, friends: newFriends}
         default:
           return state;
     }
